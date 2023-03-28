d8b1f65054cee2d4ea81734441c32bc77defcf39
+ <+>
2023-03-28 15:09:44 -0400

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+cats = ["Milo", "Otis", "Garfield"];
+
+function cats(){
+    console.log(cats);
+}
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+cats();
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+cats();
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+cats();
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const cat = [...cats, "Broom"]
+    return cat; 
+}
+
+function prependCat(){
+    const cat = ["Arnold", ...cats];
+    return cat;
+}
+
+function removeLastCat(){
+ const copyCat = cats.slice(0,2);
+ return copyCat;
+}
+
+function removeFirstCat(){
+    const anotherOne = cats.slice(1);
+    return anotherOne;
+}
